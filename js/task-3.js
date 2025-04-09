2ef560b5325d8d1e55863e16ed6fa5d2aec7446d
arda_tinmaz <tnmzarda@gmail.com>
2025-04-10 02:47:50 +0300

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+const functionInput = () => {
+    outputEl.textContent = inputEl.value.trim() || `Anonymous`;
+}
+
+inputEl.addEventListener("input",functionInput)
